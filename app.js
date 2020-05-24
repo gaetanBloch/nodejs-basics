0a32948f7126a374636f8dedf63afc04f9808973
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 20:39:02 +0200

    Export an object with the handler inside

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 const routes = require('./route')
 
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
