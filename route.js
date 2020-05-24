42a070a55a7a0a221ddbda99c1a897ef3cf618cd
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 20:41:03 +0200

    Change the way we export in the module

--- route.js
+++ route.js
@@ -33,8 +33,11 @@ const requestHandler = (req, res) => {
   }
 }
 
-module.exports = {
-  handler: requestHandler,
-  someText: 'Some hard coded text'
-};
+// module.exports = {
+//   handler: requestHandler,
+//   someText: 'Some hard coded text'
+// };
+
+module.exports.handler = requestHandler;
+module.exports.someText = 'Some hard coded text';
 
