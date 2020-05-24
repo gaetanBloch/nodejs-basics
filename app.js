418543871d7416142730b053068b7ae1243f66fe
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 20:29:21 +0200

    Use if statement instead of switch

--- app.js
+++ app.js
@@ -2,38 +2,36 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/':
-      res.write('<html>');
-      res.write('<head><title>Enter Message</title></head>');
-      res.write('<body><form action="/message" method="post">' +
-        '<input type="text" name="message"><br/><br/>' +
-        '<button type="submit">Send</button>' +
-        '</form></body>');
-      res.write('<html>');
-      return res.end();
-    case '/message':
-      if (req.method === 'POST') {
-        const body = [];
-        req.on('data', chunk => {
-          body.push(chunk);
-        });
-        return req.on('end', () => {
-          const parsedBody = Buffer.concat(body).toString();
-          const message = parsedBody.split('=')[1];
-          fs.writeFile('message.txt', message, err => {
-            res.writeHead(302, { Location: '/' });
-            return res.end();
-          });
-        })
-      }
+  if (req.url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="post">' +
+      '<input type="text" name="message"><br/><br/>' +
+      '<button type="submit">Send</button>' +
+      '</form></body>');
+    res.write('<html>');
+    return res.end();
+  } else if (req.url === '/message' && req.method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.writeHead(302, { Location: '/' });
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('<html>');
+    res.end();
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('<html>');
-  res.end();
 });
 
 server.listen(3000);
