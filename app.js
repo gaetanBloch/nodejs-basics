f3c5cd25f6c8ba1c771c9dcc52d05166e30fb406
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 16:07:07 +0200

    Setting the header of the http response

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
 });
 
 server.listen(3000);
