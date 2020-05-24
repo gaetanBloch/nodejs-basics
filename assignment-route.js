7d2ccb706a2b45a8dea491b26ff1f7875974b669
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 22:39:12 +0200

    Handle 2 routes "/" and "/users" which display dummy data

--- assignment-route.js
+++ assignment-route.js
@@ -1,5 +1,34 @@
 const requestHandler = (req, res) => {
 
-}
+  const rootHandler = () => {
+    res.write('<html>');
+    res.write('<head><title>Greetings</title></head>');
+    res.write('<body><h1>Greetings from the server!</h1></body>');
+    res.write('<html>');
+    return res.end();
+  };
+
+  const usersHandler = () => {
+    res.write('<html>');
+    res.write('<head><title>Users</title></head>');
+    res.write('<body><ul>' +
+      '<li>User 1</li>' +
+      '<li>User 2</li>' +
+      '<li>User 3</li>' +
+      '<li>User 4</li>' +
+      '</ul></body>');
+    res.write('<html>');
+    return res.end();
+  };
+
+  switch (req.url) {
+    case '/':
+      return rootHandler();
+    case '/users':
+      return usersHandler();
+    default:
+      return rootHandler();
+  }
+};
 
 module.exports = requestHandler;
