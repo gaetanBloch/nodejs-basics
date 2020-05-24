c5343fa9ee4e0c209cacd9ea548e25ca362f6574
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 19:40:18 +0200

    Return on the listener

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const server = http.createServer((req, res) => {
         req.on('data', chunk => {
           body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
           const parsedBody = Buffer.concat(body).toString();
           const message = parsedBody.split('=')[1];
           fs.writeFileSync('message.txt', message);
+          res.writeHead(302, { Location: '/' });
+          return res.end();
         })
-        res.writeHead(302, { Location: '/' });
-        return res.end();
       }
   }
   res.setHeader('Content-Type', 'text/html');
