7f258a6ff8191c924a59a2d30033a5b142847957
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-24 22:00:51 +0200

    Put Assignment instructions

---
+++ assignement-app.js
@@ -0,0 +1,9 @@
+// 1. Spin up A Node.js Server on port 3000
+// 2. Handle 2 routes: "/" and "users"
+//// a. Return some greeting on "/"
+//// b. Return a list of dummy users on "/users"
+// 3. Add a form with a username input to the "/" page and submit a POST
+// request to "/create-user" upon a button click
+// 4. Add the "/create-user" route and parse the incoming data i.e. the
+// username and log it to the console
+// 5. Redirect to "/" after logging
