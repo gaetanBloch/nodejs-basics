f89cf7c1d02c952780187ac97d4f00dd61e4053b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-29 22:55:44 +0200

    Serve root '/' route index.html page with express

--- assignement2/app.js
+++
@@ -1,4 +0,0 @@
-// 1. Create an Express.js Application which serves two html files for "/" and
-// "/users" routes.
-// 2. Add some static (.js or .css) files to the project that should be 
-// required by at least one of the HTML files.
---
+++ assignment2/app.js
@@ -0,0 +1,17 @@
+// 1. Create an Express.js Application which serves two html files for "/" and
+// "/users" routes.
+// 2. Add some static (.js or .css) files to the project that should be
+// required by at least one of the HTML files.
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.sendFile(
+    path.join(__dirname, '..', 'assignment2', 'views', 'index.html')
+  );
+});
+
+app.listen(3000);
