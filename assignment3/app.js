d5f425ba3cf82837a1c94902f03ca5ebc50e1542
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-30 22:01:45 +0200

    Created assignment application, instructions and script

---
+++ assignment3/app.js
@@ -0,0 +1,5 @@
+// 1. Create an app with Express and EJS.
+// 2. Create a route '/' which holds a <form> that allows users to input their
+// name.
+// 3. Create a route '/users' Which outputs a <ul> with the user names
+// 4. Use HTML includes to re-use some part of the code
