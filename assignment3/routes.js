aff8da33e40817d1abf0cacf1e53693293b52306
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-30 22:36:11 +0200

    POST the user name

--- assignment3/routes.js
+++ assignment3/routes.js
@@ -3,12 +3,19 @@ const path = require('path');
 
 const router = express.Router();
 
+const users = [];
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 router.get('/users', (req, res, next) => {
   res.render('users');
+});
+
+router.post('/users', (req, res, next) => {
+  users.push(req.body.user);
+  res.redirect('/users');
 });
 
 module.exports = router;
