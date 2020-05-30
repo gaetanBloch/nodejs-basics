06dd503f96ec0e1ade510be3a917a318b4c04d94
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-30 22:19:36 +0200

    Added '/' route to index page with form with user name

--- assignment3/app.js
+++ assignment3/app.js
@@ -3,3 +3,20 @@
 // name.
 // 3. Create a route '/users' Which outputs a <ul> with the user names
 // 4. Use HTML includes to re-use some part of the code
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './assignment3/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(routes);
+
+app.listen(3000);
